8e5a93e9d61810ffd857c994510fce181fff8796
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-11-09 22:37:52 +0200

    Login method added

--- ToDoList/ToDoList/Controllers/AccountController.cs
+++ ToDoList/ToDoList/Controllers/AccountController.cs
@@ -17,7 +17,7 @@ public class AccountController : BaseApiController
         _dataContext = dataContext;
     }
     
-    [HttpPost]
+    [HttpPost("register")]
     public async Task<ActionResult<User>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.UserName))
@@ -36,6 +36,26 @@ public class AccountController : BaseApiController
         await _dataContext.SaveChangesAsync();
 
         return user;
+    }
+    
+    [HttpPost("login")]
+    public async Task<ActionResult> Login(LoginDto loginDto)
+    {
+        var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.UserName);
+        if (user == null)
+            return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+                return Unauthorized("Invalid password");
+        }
+
+        return Ok("Successful login.");
     }
     
     private async Task<bool> UserExists(string userName)
