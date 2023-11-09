57c7e26ed71b250ea0e18b5e0dd6bb20ffcdb7dd
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-11-09 21:39:14 +0200

    Entities created. EF dependencies added.

---
+++ ToDoList/ToDoList/Entities/ToDo.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoList.Entities;
+
+public class ToDo
+{
+    public int Id { get; set; }
+
+    public string ToDoName { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public int UserId { get; set; }
+
+    public User User { get; set; }
+}
---
+++ ToDoList/ToDoList/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string UserName { get; set; }
+
+    public List<ToDo> ToDos { get; set; }
+}
